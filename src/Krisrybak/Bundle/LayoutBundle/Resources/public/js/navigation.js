0413659c1b68b0733e20af239be3ccdf193a9b61
krisrybak <kris.rybak@hotmail.co.uk>
2013-05-07 14:32:48 +0000

    replaced .wqhover function with .on

--- src/Krisrybak/Bundle/LayoutBundle/Resources/public/js/navigation.js
+++ src/Krisrybak/Bundle/LayoutBundle/Resources/public/js/navigation.js
@@ -1,5 +1,6 @@
 
 $("ul.nav li div span").css("display","none");
+/*
 $("ul.nav li").hover(
   function () {
     $(this).find("div").toggleClass("active");
@@ -11,8 +12,29 @@ $("ul.nav li").hover(
     $(this).find("span").fadeOut();
   }
 );
+*/
+
+$("ul.nav li").on({
+  mouseenter: function(e){
+    e.stopPropagation();
+    $(this).find("div").toggleClass("active");
+    $(this).find("span").slideToggle().fadeIn();
+    e.preventDefault();
+  },
+  mouseleave: function(e){
+    e.stopPropagation();
+    $(this).find("div").toggleClass("active");
+    $(this).find("span").fadeOut();
+    e.preventDefault();
+  },
+  click: function(e){
+    e.preventDefault();
+  }
+});
 /*
-$("ul.nav li").on("click", function(event){
-    console.log($(this).text());
+$("ul.nav li").on("click",function(e){
+    e.stopPropagation();
+
+    e.preventDefault();
 });
 */
